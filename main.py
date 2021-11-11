38598875b0a41a1c06aa89e8befdbe19ea12e972
Abhimanyu Sharma <71812498+N1nja0p@users.noreply.github.com>
2021-11-11 12:50:07 +0530

    Added sleep

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import psutil
+import time
 from notifications import notify
 BATTERY_REMINDER_PERCENT="19"
 while True:
     battery=psutil.sensors_battery()
     if str(battery.percent) == BATTERY_REMINDER_PERCENT and battery.power_plugged == False:
         notify(BATTERY_REMINDER_PERCENT,secs_left=battery.secsleft,event="battery-low")
-        break
+        time.sleep(60)
     if battery.power_plugged:
         notify(battery.percent,"plugged-in")
-        break
+        time.sleep(60)
     if str(battery.percent) == "100":
         notify(battery.percent,event="fully-charged")
-        break
+        time.sleep(60)
