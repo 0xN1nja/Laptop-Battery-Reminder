5064848cd7aa0ebcc06d85f35ebe34c97033e22e
Abhimanyu Sharma <71812498+N1nja0p@users.noreply.github.com>
2021-11-11 20:49:20 +0530

    Converted BATTERY_REMIDER_PERCENT To Int

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from notifications import notify
 BATTERY_REMINDER_PERCENT="19"
 while True:
     battery=psutil.sensors_battery()
-    if str(battery.percent) == BATTERY_REMINDER_PERCENT and battery.power_plugged == False:
+    if int(battery.percent) <= int(BATTERY_REMINDER_PERCENT) and battery.power_plugged == False:
         notify(BATTERY_REMINDER_PERCENT,secs_left=battery.secsleft,event="battery-low")
         time.sleep(60)
     if battery.power_plugged:
         notify(battery.percent,"plugged-in",secs_left=battery.secsleft)
         time.sleep(60)
-    if str(battery.percent) == "100":
+    if int(battery.percent) == 100:
         notify(battery.percent,event="fully-charged",secs_left=battery.secsleft)
         time.sleep(60)
-    if str(battery.percent) == "9":
+    if int(battery.percent) <= 9:
         notify(battery.percent,event="critically-low",secs_left=battery.secsleft)
         time.sleep(60)
