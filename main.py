031134b2b3ef5073db47f7b5fbaf452674e8a1c7
Abhimanyu Sharma <71812498+N1nja0p@users.noreply.github.com>
2021-11-11 12:41:23 +0530

    Useless Notification Bug Fixed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ BATTERY_REMINDER_PERCENT="19"
 while True:
     battery=psutil.sensors_battery()
     if str(battery.percent) == BATTERY_REMINDER_PERCENT and battery.power_plugged == False:
-        notify(BATTERY_REMINDER_PERCENT,secs_left=battery.secsleft)
+        notify(BATTERY_REMINDER_PERCENT,secs_left=battery.secsleft,event="battery-low")
         break
     if battery.power_plugged:
         notify(battery.percent,"plugged-in")
